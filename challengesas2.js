f1d7f630076afc84e2a2cafe165d8a414eea797a
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-17 11:08:46 +0100

    challenge

---
+++ challengesas2.js
@@ -0,0 +1,5 @@
+const nomEtPrenom = "ikram arbaoui"
+const age = 19
+const sexe = "fille"
+const adresse mail = "ikramarbaoui@gmail.com"
+console.log(`je suis ${nom et prenome}, j’ai ${age} ans, mon email est  ${adresse mail}.`);
